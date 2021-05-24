33ab237a2f1c975f55aeacf4857fe9f658f0d590
chenyaolei <73279168+chenyaolei@users.noreply.github.com>
2021-05-24 10:54:08 +0800

    远程库在线更新

--- HelloGit/src/main/java/test01.java
+++ HelloGit/src/main/java/test01.java
@@ -5,6 +5,6 @@ public class test01 {
         System.out.println("创建分支fork1");
         System.out.println("主干master更新1");
         System.out.println("分支fork1更新1");
-
+        System.out.println("远程库更新1");
     }
 }
